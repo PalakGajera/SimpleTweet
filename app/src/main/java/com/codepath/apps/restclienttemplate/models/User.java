365b7f8ba06a138d2fcac2ea796b957560f983e0
PalakGajera <pgajera@uci.edu>
2019-11-08 20:16:42 -0800

    Part 2: Added Compose tweet file, users can compose, post and view tweet now!

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -2,12 +2,20 @@ package com.codepath.apps.restclienttemplate.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class User {
 
     public String name;
     public String screename;
     public String profileImageUrl;
+
+    //empty constructor needed by the Parceler Library
+    public User()
+    {
+
+    }
     //parse and get info from jsonobject
     public static User  fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
